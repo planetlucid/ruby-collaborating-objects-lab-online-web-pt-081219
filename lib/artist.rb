65e47e183b7fdb2b2ccb5b372ddb2e759ecd8c35
Ryan S <planetlucid@gmail.com>
2019-09-22 19:08:46 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Song
+  attr_accessor :title
+ 
+  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
+  end
+ 
+end
+ 
+class MP3Importer
+  def import(list_of_filenames)
+    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  end
+end
